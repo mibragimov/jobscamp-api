3d7e46c20011973be73cdb099719cb42d42f8870
Muhammad Ibragimov <mibragimov@hotmail.com>
2020-06-24 17:08:33 +0500

    add static file to serve at home route

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 require("./db/mongoose");
@@ -7,7 +8,7 @@ const companyRoutes = require("./routers/company");
 const jobRoutes = require("./routers/job");
 
 const port = process.env.PORT;
-
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(companyRoutes);
 app.use(jobRoutes);
